b366bf3949f7c4387762cdc5980a5e8b27f91be5
Karson Kalt <kars.kalt@gmail.com>
2024-12-05 15:41:33 -0500

    prettier

--- src/BackgroundImageSelector/index.js
+++ src/BackgroundImageSelector/index.js
@@ -1,30 +1,47 @@
 import "./styles.css";
 import React, { useState } from "react";
 
-const backgroundImageOptions = [{ label: "city", path: "./assets/city.png" },
-{ label: "mountain", path: "./assets/mountain.png" },
-{ label: "sea", path: "./assets/sea.png" },
-{ label: "village", path: "./assets/village.png" }];
+const backgroundImageOptions = [
+  { label: "city", path: "./assets/city.png" },
+  { label: "mountain", path: "./assets/mountain.png" },
+  { label: "sea", path: "./assets/sea.png" },
+  { label: "village", path: "./assets/village.png" },
+];
 
-export const BackgroundImageSelector = ({onImageClick}) => {
-    const [isVisible,setVisibility]=useState(false);
-    const toggleVisibility = () =>{
-        setVisibility((prev)=>!prev);
-    };
-    return (
-        <>
-        <div className="toggle-button fa-solid fa-image"  onClick={toggleVisibility}></div>
-        {isVisible &&(
-            <div className="selection-preview">
-            {backgroundImageOptions.map((option) => {
-                return <div key={option.name} className="background-image-selector__root" onClick={()=>onImageClick(option.label)}>
-                    <img src={option.path} className="background-image-selector__thumbnail" ></img>
-                    <span className="background-image-selector__label">{option.label}</span>
-                </div>
-            })}
-            </div>
-        )}
-        
-        </>
-    )
-}
+export const BackgroundImageSelector = ({ onImageClick }) => {
+  const [isVisible, setVisibility] = useState(false);
+
+  const toggleVisibility = () => {
+    setVisibility((prev) => !prev);
+  };
+
+  return (
+    <>
+      <div
+        className="toggle-button fa-solid fa-image"
+        onClick={toggleVisibility}
+      />
+      {isVisible && (
+        <div className="selection-preview">
+          {backgroundImageOptions.map((option) => {
+            return (
+              <div
+                key={option.name}
+                className="background-image-selector__root"
+                onClick={() => onImageClick(option.label)}
+              >
+                <img
+                  src={option.path}
+                  className="background-image-selector__thumbnail"
+                />
+                <span className="background-image-selector__label">
+                  {option.label}
+                </span>
+              </div>
+            );
+          })}
+        </div>
+      )}
+    </>
+  );
+};
